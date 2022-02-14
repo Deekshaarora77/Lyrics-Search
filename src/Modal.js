94856834d93ac6f5a6793966ad692ab56aca38bd
Deeksha <deeksha.arora77@gmail.com>
2022-02-14 22:10:43 +0530

    mobile responsive

--- src/Modal.js
+++ src/Modal.js
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from "react";
 
 const Modal = ({ setIsOpen, modalState }) => {
     const [lyrics, setLyrics] = useState("")
+    const [error, seterror] = useState(false)
     const fetchLyrics = async () => {
         const res = await fetch(`https://api.lyrics.ovh/v1/${modalState.artist}/${modalState.song}`)
         const status = res.status
         if (status === 404) {
             setLyrics(<><img className="errorMessage" src="https://mir-s3-cdn-cf.behance.net/project_modules/fs/d7e893109649775.5fd8be208ede7.jpg" /><p>Lyrics not found for this song :(</p></>)
+            seterror(true)
             return
         }
+        seterror(false)
         const result = await res.json()
         const newLyrics = result.lyrics.split('\n').map(str => <p>{str}</p>)
         setLyrics(newLyrics)
@@ -18,14 +21,14 @@ const Modal = ({ setIsOpen, modalState }) => {
     }, [])
 
     return (
-        <>
-            <button className="modButton" onClick={() => setIsOpen(false)} >X</button>
-            <div className="modalx">
+        <div className="modal-container">
+            <button className="modButton" id={error?"error-button":undefined} onClick={() => setIsOpen(false)} >X</button>
+            <div className="modalx" id={error?"error-modal":undefined}>
                 <div className="lyrics">
                     <p>{lyrics}</p>
                 </div >
             </div>
-        </>
+        </div>
     );
 };
 
